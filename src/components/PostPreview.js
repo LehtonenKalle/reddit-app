ff167829da51348d0a935267c273367274867d7f
Kalle Lehtonen <kalle.lehtonen01@gmail.com>
2022-03-04 00:58:38 +0200

    improve the style of the page

--- src/components/PostPreview.js
+++ src/components/PostPreview.js
@@ -1,24 +1,24 @@
-import comment from '../assets/images/comment-black.png';
-import upArrow from '../assets/images/up-arrow.png'
+import comment from '../assets/images/comment.png';
+import like from '../assets/images/like.png'
 
 export default function PostPreview({ postData }) {
   return (
     <button className='post-container'>
       <div className='post-thumbnail-container'>
-        <h2>{postData.title}</h2>
+        <h2 className='post-title'>{postData.title}</h2>
         <h3 className='subreddit'>{postData.subreddit_name_prefixed}</h3>
         <img className='thumbnail' src={postData.url} alt=''/>
       </div>
       <div className='post-info-container'>
         <div className='score-container'>
-          <img className='upvote' src={upArrow} alt='upvote icon'/>
-          <h3 className='score'>{postData.score}</h3>
+          <img className='like' src={like} alt='like icon'/>
+          <h4 className='score'>{postData.score}</h4>
         </div>
         <div className='comments-container'>
           <img className='comment' src={comment} alt='comment icon'/>
-          <h3 className='comments'>{postData.num_comments}</h3>
+          <h4 className='comments'>{postData.num_comments}</h4>
         </div>
-        <h3 className='author'>Posted by {postData.author}</h3>
+        <h4 className='author'>Posted by {postData.author}</h4>
       </div>
     </button>
   )
