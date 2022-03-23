23682734ef96df00956c5a0f44fe15834a085e03
Kalle Lehtonen <kalle.lehtonen01@gmail.com>
2022-03-23 22:16:18 +0200

    improved routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation, useParams } from 'react-router-dom';
 
 import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
@@ -9,23 +9,17 @@ import Header from './features/header/Header';
 import Subreddits from './features/subreddits/Subreddits';
 
 function App() {
-
   return (
-    <>
-      <Router>
-        <Header />
-        <main>
-          <Routes>
-            <Route path="/" element={<PostPreviews />} />
-            <Route path="/hot" element={<PostPreviews page='hot' />} />
-            <Route path="/top" element={<PostPreviews page='top' />} />
-            <Route path="/new" element={<PostPreviews page='new' />} />
-            <Route path="/rising" element={<PostPreviews page='rising' />} />
-          </Routes>
-          <Subreddits />
-        </main>
-      </Router>
-    </>
+    <Router>
+      <Header />
+      <main>
+        <Routes>
+          <Route path="/*" element={<PostPreviews />}/>
+        </Routes>
+        <Subreddits />
+      </main>
+    </Router>
+
   );
 }
 
--- src/features/postPreviews/posts/PostPreviews.js
+++ src/features/postPreviews/posts/PostPreviews.js
@@ -4,15 +4,19 @@ import { useDispatch, useSelector } from 'react-redux';
 import { isLoading, loadPostPreviews, selectAllPreviews } from './postPreviewsSlice';
 import PostPreview from '../../../components/PostPreview';
 import '../../../styles/PostPreviews.css'
+import { useParams } from 'react-router-dom';
 
 const PostPreviews = ({ page }) => {
+  const params = useParams();
+  const path = params['*'];
+
   const dispatch = useDispatch();
   const isLoadingPreviews = useSelector(isLoading);
   const postPreviews = useSelector(selectAllPreviews);
 
   useEffect(() => {
-    dispatch(loadPostPreviews(page));
-  }, [page])
+    dispatch(loadPostPreviews(path));
+  }, [path]);
 
   if (isLoadingPreviews) {
     return (
